e4aeb77d76e9ec459da6fa18d031ae2069d8dfc4
sangwopark19 <sangwopark19@gmail.com>
2024-11-25 16:34:23 +0900

    git init

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div>
-      <div className="bg-blue-500 text-white text-center p-10">
-        <p className="text-emerald-800">asdfasdfa</p>
-      </div>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
