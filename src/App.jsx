0dc238cf3269bc1e7ce66a9b7f51ea8933b19d10
sangwopark19 <sangwopark19@gmail.com>
2024-11-29 12:13:17 +0900

    feat: App.jsx Home, Login 페이지 라우팅 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
