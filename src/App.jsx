a55de5e7274ace3fabaa240dbcde9b0a9e471fe6
shb <shb2891@gmail.com>
2024-11-29 12:25:50 +0900

    feat: App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
-
+import CourseReviewForm from "./component/course/Course";
 function App() {
-  return <></>;
+  return <>
+  <CourseReviewForm></CourseReviewForm></>;
 }
 
 export default App;
