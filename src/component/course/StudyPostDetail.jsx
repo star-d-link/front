25347c13cf3127e2ab30ddb05d82356d82fd18e6
shb <shb2891@gmail.com>
2024-12-18 10:52:08 +0900

    fix: 작성자가 아니면 수정, 삭제버튼이 안나오게 수정

--- src/component/course/StudyPostDetail.jsx
+++ src/component/course/StudyPostDetail.jsx
@@ -3,8 +3,10 @@ import { useParams, useNavigate } from "react-router-dom";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css"; // Quill 기본 스타일 추가
 import apiClient from "../../auth/apiClient";
+import { useAuth } from "../../auth/AuthContext.jsx";
 
 const StudyPostDetail = () => {
+  const { user } = useAuth(); // 현재 로그인 사용자 정보
   const { studyId, postId } = useParams();
   const navigate = useNavigate();
   const [post, setPost] = useState(null);
@@ -18,6 +20,7 @@ const StudyPostDetail = () => {
           `/study/group/${studyId}/post/${postId}`
         );
         setPost(response.data);
+        console.log(user);
       } catch (err) {
         setError("게시글을 불러오는 데 실패했습니다.");
       } finally {
@@ -44,7 +47,7 @@ const StudyPostDetail = () => {
   const handleBackToList = () => {
     navigate(`/study/group/${studyId}/list`); // 리스트 페이지로 이동
   };
-  
+  const isOwner = user && post && user.username === post.author;
   
 
   if (loading) return <div>로딩 중...</div>;
@@ -73,6 +76,7 @@ const StudyPostDetail = () => {
         >
           목록
         </button>
+        {isOwner && (<>
         <button
           onClick={handleEdit}
           className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
@@ -84,7 +88,8 @@ const StudyPostDetail = () => {
           className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
         >
           삭제
-        </button>
+        </button></>
+        )}
       </div>
     </div>
   );
